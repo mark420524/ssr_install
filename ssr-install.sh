ff7dce05d8a68aa99312dd197564ddfd39f1e755
mark <markjava@foxmail.com>
2019-04-07 21:24:23 +0800

    修改安装连接

--- ssr-install.sh
+++ ssr-install.sh
@@ -84,7 +84,7 @@ cd /root/
 rm -rf libsodium-1.0.16.tar.gz libsodium-1.0.16
 
 #git安装ssr
-git clone https://github.com/91yun/shadowsocksr-1.git ${ssrdir}
+git clone https://github.com/mark420524/shadowsocksr.git ${ssrdir}
 cd ${ssrdir}
 bash setup_cymysql.sh
 bash initcfg.sh
